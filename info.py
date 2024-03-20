74e897b450346cf717b8737bc80b5bce353deacc
TeluguZone <140744388+TeluguZone@users.noreply.github.com>
2024-03-21 00:10:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -103,8 +103,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://krnti-098ea5d05a8e.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://krnti-098ea5d05a8e.herokuapp.com/".format(FQDN, PORT)
+URL = "https://daemonx-7420dc73c049.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://daemonx-7420dc73c049.herokuapp.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -119,9 +119,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',True))
 if HAS_SSL:
-    URL = "https://krnti-098ea5d05a8e.herokuapp.com/".format(FQDN)
+    URL = "https://daemonx-7420dc73c049.herokuapp.com/".format(FQDN)
 else:
-    URL = "https://krnti-098ea5d05a8e.herokuapp.com/".format(FQDN)
+    URL = "https://daemonx-7420dc73c049.herokuapp.com/".format(FQDN)
 
 # add premium logs channel id
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002014827868'))
